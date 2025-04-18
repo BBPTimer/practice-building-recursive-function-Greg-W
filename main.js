980da15a8cd5043e269decc8af4844392981cb59
gweseloh <greg.weseloh@gmail.com>
2025-04-18 07:48:04 -0500

    Created recursive function to reverse a string

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+function reverseString(inputString) {
+    if (inputString.length === 1 || inputString.length === 0) {
+        return inputString;
+    }
+    return inputString.slice(-1) + reverseString(inputString.slice(0, -1));
+}
 
+console.log(reverseString("hello")); // Outputs: "olleh"
+console.log(reverseString("recursion")); // Outputs: "noisrucer"
+console.log(reverseString("a")); // Outputs: "a"
+console.log(reverseString("")); // Outputs: ""
